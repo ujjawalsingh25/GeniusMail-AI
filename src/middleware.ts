b05302dde5b6faef29d133f20c0f9a1cc48c7344
Ujjawal Kumar <ujjawalsingh800007@gmail.com>
2024-10-19 14:38:16 +0530

    Clerk Webhook and Sync Data with DB

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher([
-  '/sign-in(.*)', 
+  '/sign-in(.*)',
   '/sign-up(.*)',
+  '/api/clerk/webhook(.*)',
 ])
 
 export default clerkMiddleware((auth, request) => {
